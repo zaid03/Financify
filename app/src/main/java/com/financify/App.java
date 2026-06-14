b44f7c51f0fb98a0e3020c9cddde7f9f95014cc2
zaid03 <terguyzaid@gmail.com>
2026-06-14 02:09:12 +0100

    first init

---
+++ app/src/main/java/com/financify/App.java
@@ -0,0 +1,23 @@
+package com.financify;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        var root = new StackPane(new Label("Financify Running"));
+
+        stage.setScene(new Scene(root, 800, 600));
+        stage.setTitle("Financify");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
